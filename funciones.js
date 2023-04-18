1b215d485090fc3ed410fc1b94fcd49a1c952a2d
MATIAS GOLONBEK <47343179@ort>
2023-04-18 10:41:56 -0300

    jajsj

--- funciones.js
+++ funciones.js
@@ -15,13 +15,42 @@ function CargarPokemones() {
     .then((result) => {
       const pokemones = result.data.results;
 
-      pokemones.map((pokemon) => {
+      pokemones.map((pokemon, index) => {
         const { name, url } = pokemon;
 
-        document.querySelector("#listado").innerHTML += `<li>${name}</li>`;
+        document.querySelector("#listado").innerHTML += `
+        
+        <div class="card border-success mb-3" style="max-width: 20rem;">
+        <div class="card-header bg-transparent border-success">${name} ${index+1}</div>
+        <div class="card-body text-success card-group">
+          <img src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${index+1}.svg">
+          
+        </div>
+        <div class="card-footer bg-transparent border-success">   
+        <button onclick="DetallePokemon('${url}')">Ver detalle</button>
+        <ulid="detalle"></ul>
+          </div>
+      </div>
+      `;
       });
     })
     .catch((error) => {
       console.log(error);
     });
 }
+function DetallePokemon(url) {
+  axios
+  .get(url)
+  .then((result) => {
+    console.log(result.data);
+    Habilidades = result.data.abilities;
+    Estadisticas = result.data.stats;
+    document.querySelector("#detalle").innerHTML += `${Habilidades}
+    ${Estadisticas}`
+
+
+    
+  });
+
+}
+

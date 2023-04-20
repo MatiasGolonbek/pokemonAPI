35623b3eef9a00fbd8a402d2ded8419af58245c0
MATIAS GOLONBEK <47343179@ort>
2023-04-20 08:53:51 -0300

    falta diseño

--- funciones.js
+++ funciones.js
@@ -20,15 +20,20 @@ function CargarPokemones() {
 
         document.querySelector("#listado").innerHTML += `
         
-        <div class="card border-success mb-3" style="max-width: 20rem;">
-        <div class="card-header bg-transparent border-success">${name} ${index+1}</div>
-        <div class="card-body text-success card-group">
+         <div class="col-sm-6">
+          <div class="card border-success mb-3" style="max-width: 20rem;">
+            <div class="card-header bg-transparent border-success">${name} ${index+1}</div>
+             <div class="card-body text-success card-group">
           <img src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${index+1}.svg">
           
         </div>
-        <div class="card-footer bg-transparent border-success">   
+        <div class="card-footer bg-transparent border-success"> 
+        <div id="${url}"> 
         <button onclick="DetallePokemon('${url}')">Ver detalle</button>
-        <ulid="detalle"></ul>
+        </div>
+        <ul id="detalle"></ul>
+              </div>
+            </div>
           </div>
       </div>
       `;
@@ -41,16 +46,20 @@ function CargarPokemones() {
 function DetallePokemon(url) {
   axios
   .get(url)
-  .then((result) => {
-    console.log(result.data);
-    Habilidades = result.data.abilities;
-    Estadisticas = result.data.stats;
-    document.querySelector("#detalle").innerHTML += `${Habilidades}
-    ${Estadisticas}`
+  .then((result) => {    
+      const { abilities, stats } = result.data;
+      console.log(stats);
+      document.querySelector("#detalle").innerHTML += `<p> Habilidades</p>`;
+      abilities.map(item=>{
+        document.querySelector("#detalle").innerHTML += `<p>- ${item.ability.name}</p>`;
+      })
+      document.querySelector("#detalle").innerHTML += `<p> Estadisticas</p>`;
+      stats.map(item=>{
+        document.querySelector("#detalle").innerHTML += `<p>- ${item.stat.name}:  ${item.base_stat}</p>`;
+      })
 
 
-    
-  });
-
+      
+    }); 
 }
 
